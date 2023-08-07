e02cff6fe98c59050d07dc32c3df0d9666bc849a
Nnee <dvn281002@gmail.com>
2023-08-07 11:39:15 +0700

    fix: remove contract write

--- src/modules/contract/contract.module.ts
+++ src/modules/contract/contract.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common"
 import { ContractReadController } from "./controllers/contract-read.controller"
-import { ContractWriteController } from "./controllers/contract-write.controller"
 import { ContractReadService } from "./services/contract-read.service"
-import { ContractWriteService } from "./services/contract-write.service"
 
 @Module({
-  controllers: [ContractReadController, ContractWriteController],
-  providers: [ContractReadService, ContractWriteService],
+  controllers: [ContractReadController],
+  providers: [ContractReadService],
 })
 export class ContractModule {}
