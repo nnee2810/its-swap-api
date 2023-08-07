24e4476b4dbc938da59d69c8a002477aa6b588b0
Nnee <dvn281002@gmail.com>
2023-08-07 10:42:50 +0700

    fix: eslint warning

--- src/modules/contract/controllers/contract-write.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@nestjs/common"
-import { ApiTags } from "@nestjs/swagger"
-import { ContractWriteService } from "../services/contract-write.service"
-
-@ApiTags("Contract Write")
-@Controller("contract/write")
-export class ContractWriteController {
-  constructor(private contractService: ContractWriteService) {}
-}
